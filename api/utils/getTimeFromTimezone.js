a3f8bb9d2d4a517049ef003171d7cf6198fbf889
José Camacho <j.d.camacho27@gmail.com>
2022-05-31 19:35:08 -0400

    hotfix for the hourx2

--- api/utils/getTimeFromTimezone.js
+++ api/utils/getTimeFromTimezone.js
@@ -3,6 +3,6 @@ export const getTimeFromTimezone = (timezone) => {
   const time = date.getTime()
   const offset = date.getTimezoneOffset() * 60000
   const utc = time + offset
-  const newDate = new Date(utc + (1000 * timezone))
+  const newDate = new Date(utc + (3600000 * timezone))
   return newDate
 }

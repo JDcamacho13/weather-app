57cd1cd1f548386cc1540591418f56b059da10a2
José Camacho <j.d.camacho27@gmail.com>
2022-05-31 21:56:13 -0400

    hotfix

--- api/weather.js
+++ api/weather.js
@@ -44,105 +44,118 @@ const getWeather = async ({ name, country }) => {
 }
 
 
-const mooked = (req, res) => {
+export default (req, res) => {
 
   res.status(200).json(
     {
       "weatherToday": {
-        "id": 802,
-        "icon": "03n",
-        "weather": "nubes dispersas",
-        "temp": 26.08,
+        "id": 804,
+        "icon": "04n",
+        "weather": "nubes",
+        "temp": 22.23,
         "wind": {
-          "speed": 3.09,
-          "deg": 110
+          "speed": 1.69,
+          "deg": 17,
+          "gust": 2.24
         },
-        "humidity": 78,
+        "humidity": 73,
         "visibility": 10000,
         "pressure": 1013
       },
       "weatherForecast": [
         {
-          "id": 801,
-          "icon": "02d",
-          "weather": "algo de nubes",
-          "temp": 27.4,
-          "wind": {
-            "speed": 2.91,
-            "deg": 86,
-            "gust": 3.69
-          },
-          "humidity": 72,
-          "visibility": 10000,
-          "pressure": 1013,
-          "date": "2022-05-31 12:00:00"
-        },
-        {
-          "id": 803,
+          "id": 804,
           "icon": "04d",
-          "weather": "muy nuboso",
-          "temp": 27.86,
+          "weather": "nubes",
+          "temp": 24.61,
+          "temp_min": 24.61,
+          "temp_max": 24.61,
           "wind": {
-            "speed": 1.61,
-            "deg": 99,
-            "gust": 2.53
+            "speed": 0.45,
+            "deg": 190,
+            "gust": 0.66
           },
-          "humidity": 71,
+          "humidity": 63,
           "visibility": 10000,
-          "pressure": 1013,
+          "pressure": 1014,
           "date": "2022-06-01 12:00:00"
         },
         {
           "id": 804,
           "icon": "04d",
           "weather": "nubes",
-          "temp": 27.81,
+          "temp": 25.82,
+          "temp_min": 25.82,
+          "temp_max": 25.82,
           "wind": {
-            "speed": 1.38,
-            "deg": 98,
-            "gust": 2.72
+            "speed": 1.03,
+            "deg": 130,
+            "gust": 1.8
           },
-          "humidity": 68,
+          "humidity": 62,
           "visibility": 10000,
-          "pressure": 1013,
+          "pressure": 1014,
           "date": "2022-06-02 12:00:00"
-        },
-        {
-          "id": 802,
-          "icon": "03d",
-          "weather": "nubes dispersas",
-          "temp": 27.61,
-          "wind": {
-            "speed": 2.06,
-            "deg": 91,
-            "gust": 3.21
-          },
-          "humidity": 75,
-          "visibility": 10000,
-          "pressure": 1013,
-          "date": "2022-06-03 12:00:00"
         },
         {
           "id": 500,
           "icon": "10d",
           "weather": "lluvia ligera",
-          "temp": 27.16,
+          "temp": 24.71,
+          "temp_min": 24.71,
+          "temp_max": 24.71,
           "wind": {
-            "speed": 0.96,
-            "deg": 127,
-            "gust": 2.61
+            "speed": 1.14,
+            "deg": 157,
+            "gust": 1.93
           },
-          "humidity": 84,
+          "humidity": 77,
           "visibility": 10000,
-          "pressure": 1013,
+          "pressure": 1014,
+          "date": "2022-06-03 12:00:00"
+        },
+        {
+          "id": 804,
+          "icon": "04d",
+          "weather": "nubes",
+          "temp": 23.82,
+          "temp_min": 23.82,
+          "temp_max": 23.82,
+          "wind": {
+            "speed": 1.19,
+            "deg": 136,
+            "gust": 2.3
+          },
+          "humidity": 83,
+          "visibility": 10000,
+          "pressure": 1014,
           "date": "2022-06-04 12:00:00"
+        },
+        {
+          "id": 500,
+          "icon": "10d",
+          "weather": "lluvia ligera",
+          "temp": 24.17,
+          "temp_min": 24.17,
+          "temp_max": 24.17,
+          "wind": {
+            "speed": 1.2,
+            "deg": 125,
+            "gust": 2.04
+          },
+          "humidity": 82,
+          "visibility": 10000,
+          "pressure": 1014,
+          "date": "2022-06-05 12:00:00"
         }
       ],
-      "country": "Venezuela"
+      "country": "Venezuela",
+      "city": "Camburito",
+      "time": "2020-10-09T05:54:56.882Z"
     })
 }
 
-export default async (req, res) => {
+const realData = async (req, res) => {
   try {
     const results = {
       weatherToday: {},

8e5f315d6c976f21412534afc10d8267ce17d25e
José Camacho <j.d.camacho27@gmail.com>
2022-06-02 00:33:50 -0400

    hotfix

--- api/utils/getTimeFromTimezone.js
+++ api/utils/getTimeFromTimezone.js
@@ -1,6 +1,6 @@
 export const getTimeFromTimezone = (timezoneOffset) => {
   const time = new Date()
   const utc = time.getTime() + (time.getTimezoneOffset() * 60000)
-  const nd = new Date(utc + (1000 * timezoneOffset))
+  const nd = new Date(utc + (3600000 * timezoneOffset))
   return nd
 }
